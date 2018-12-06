7f353312d9cf878e47ba3988b64536e6719edadb
zhoujiaping <421211679@qq.com>
2018-12-06 15:46:15 +0800

    runtime

--- src/main/java/org/jsirenia/util/runtime/RuntimeUtil.java
+++ src/main/java/org/jsirenia/util/runtime/RuntimeUtil.java
@@ -2,18 +2,10 @@ package org.jsirenia.util.runtime;
 
 public class RuntimeUtil {
 	/**
-	 * 获取当前执行代码所在的类和方法
-	 * 注意：必须直接调用该方法，否则结果会不正确！！！
 	 * @return
 	 */
-    public static StackTraceElement currentStackTraceElement() {
+    public static StackTraceElement[] stackTraceElements() {
     	Exception e = new Exception();
-    	StackTraceElement ele = e.getStackTrace()[1];
-    	return ele;
+    	return e.getStackTrace();
     }
-    public static void main(String[] args) {
-    	StackTraceElement ele = currentStackTraceElement();
-    	System.out.println(ele.getClassName());
-    	System.out.println(ele.getMethodName());
-	}
 }
