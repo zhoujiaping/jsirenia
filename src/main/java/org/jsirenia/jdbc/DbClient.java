8383e2ae8fb4ab5d34abc012f1224385f468425f
zhoujiaping <421211679@qq.com>
2019-07-08 20:46:51 +0800

    11

--- src/main/java/org/jsirenia/jdbc/DbClient.java
+++ src/main/java/org/jsirenia/jdbc/DbClient.java
@@ -5,11 +5,13 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Savepoint;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.Stack;
+import java.util.stream.Collectors;
 
 import javax.sql.DataSource;
 
@@ -22,44 +24,29 @@ import org.jsirenia.util.Callback.Callback11;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
 public class DbClient {
+	private static final List<Object> emptyObjectList = new ArrayList<>(0);
 	private static final Logger logger = LoggerFactory.getLogger(DbClient.class);
-	private ThreadLocal<Connection> connectionHolder = new ThreadLocal<>();
-	private Map<String, Savepoint> savePoints = new ConcurrentHashMap<>();
-	/*
-	 * 是否在withConn方法中。withTx需要判断，如果是在withConn方法中，
-	 * 那么withTx就不能打开新的连接，只能使用原来的连接。也不能关闭连接。 如果是不是在withConn中运行，那么它需要关闭连接。
-	 */
-	private boolean inWithConn = false;
-	private boolean inWithTx = false;
+	private ThreadLocal<Stack<Connection>> connStackHolder = new ThreadLocal<>();
 	private DataSource ds;
 
 	public DbClient(DataSource ds) {
 		this.ds = ds;
 	}
 
-	private Connection openConnection() throws SQLException {
-		Connection conn = connectionHolder.get();
-		if (conn != null) {
-			throw new RuntimeException("当前线程已经有连接");
-		}
-		conn = ds.getConnection();
+	private Connection openConn() throws SQLException {
+		Connection conn = ds.getConnection();
 		conn.setTransactionIsolation(Connection.TRANSACTION_REPEATABLE_READ);
 		if (conn.getAutoCommit()) {
 			conn.setAutoCommit(false);
 		}
-		connectionHolder.set(conn);
 		return conn;
 	}
 
-	/**
-	 * 打开连接，执行回调，关闭连接
-	 * 
-	 * @param callback
-	 */
 	public void withConn(Callback10<Connection> callback) {
 		withConn((conn) -> {
 			callback.apply(conn);
@@ -67,16 +54,17 @@ public class DbClient {
 		});
 	}
 
-	/**
-	 * 打开连接，执行回调，关闭连接
-	 * 
-	 * @param callback
-	 */
 	public <R> R withConn(Callback11<R, Connection> callback) {
 		Connection conn = null;
 		try {
-			inWithConn = true;
-			conn = openConnection();
+			Stack<Connection> stack = connStackHolder.get();
+			if (stack == null) {
+				stack = new Stack<>();
+				connStackHolder.set(stack);
+			}
+			conn = openConn();
+			logger.debug("线程{}已打开{}个连接", Thread.currentThread().getName(), stack.size());
+			stack.push(conn);
 			R res = callback.apply(conn);
 			commit(conn);
 			return res;
@@ -84,8 +72,15 @@ public class DbClient {
 			rollback(conn);
 			throw new RuntimeException(e);
 		} finally {
-			connectionHolder.remove();
-			inWithConn = false;
+			Stack<Connection> stack = connStackHolder.get();
+			if (stack != null && !stack.isEmpty()) {
+				if (stack.peek() == conn) {
+					stack.pop();
+				}
+				logger.debug("线程{}还有{}个连接", Thread.currentThread().getName(), stack.size());
+			} else {
+				logger.debug("线程{}还有{}个连接", Thread.currentThread().getName(), 0);
+			}
 			close(conn);
 		}
 	}
@@ -126,18 +121,6 @@ public class DbClient {
 		}
 	}
 
-	private void rollback(Connection conn, Savepoint point) {
-		if (conn != null) {
-			try {
-				if (!conn.isClosed() && !conn.getAutoCommit() && point != null) {
-					conn.rollback(point);
-				}
-			} catch (SQLException e) {
-				throw new RuntimeException(e);
-			}
-		}
-	}
-
 	public void withTx(Callback00 callback) {
 		withTx(() -> {
 			callback.apply();
@@ -146,99 +129,122 @@ public class DbClient {
 	}
 
 	private <T> T withTxInternal(Callback01<T> callback) {
-		Connection conn = null;
-		String pointName = UUID.randomUUID().toString();
+		Stack<Connection> stack = connStackHolder.get();
+		Connection conn = stack.peek();
 		try {
-			inWithTx = true;
-			conn = connectionHolder.get();
-			Savepoint point = conn.setSavepoint(pointName);
-			savePoints.put(pointName, point);
 			T res = callback.apply();
 			commit(conn);
 			return res;
 		} catch (Exception e) {
-			Savepoint point = savePoints.remove(pointName);
-			rollback(conn,point);
+			rollback(conn);
 			throw new RuntimeException(e);
-		} finally {
-			savePoints.remove(pointName);
-			inWithTx = false;
 		}
 	}
 
 	public <T> T withTx(Callback01<T> callback) {
-		if (!inWithConn) {
-			return withConn(conn -> {
-				return withTxInternal(callback);
-			});
+		if (hasConn()) {
+			return withTxInternal(callback);
 		}
-		return withTxInternal(callback);
+		return withConn(conn -> {
+			return withTxInternal(callback);
+		});
 	}
 
-	public JSONObject queryOne(String sql,JSONObject params) {
-		JSONArray array = query(sql, params);
+	private boolean hasConn() {
+		Stack<Connection> stack = connStackHolder.get();
+		return stack != null && !stack.isEmpty();
+	}
+
+	public <T> T queryUnique(String sql, Class<T> clazz, Object... params) {
+		Map<String, Object> map = queryUniqueMap(sql, params);
+		JSONObject json = (JSONObject) JSON.toJSON(map);
+		return json.toJavaObject(clazz);
+	}
+
+	public Map<String, Object> queryUniqueMap(String sql, Object... params) {
+		List<Map<String, Object>> array = queryMap(sql, params);
 		int size = array.size();
 		if (size == 0) {
 			return null;
 		} else if (size > 1) {
 			throw new RuntimeException("查询结果" + size + "条，预期1条");
 		}
-		return array.getJSONObject(0);
-	}
-	public JSONObject queryOne(String sql) {
-		return queryOne(sql,null);
+		return array.get(0);
 	}
 
-	/**
-	 * 执行查询。必须在withTx或者withConn方法的回调中执行
-	 * 
-	 * @param sql
-	 * @return
-	 */
-	public JSONArray query(String sql) {
-		return query(sql, null);
+	public <T> T queryUniqueByMap(String sql, Class<T> clazz, Map<String, ?> params) {
+		Map<String, Object> map = queryUniqueMapByMap(sql, params);
+		JSONObject json = (JSONObject) JSON.toJSON(map);
+		return json.toJavaObject(clazz);
 	}
 
-	public JSONArray query(String sql, JSONObject params) {
-		if (!inWithConn && !inWithTx) {
-			String sql0 = sql;
-			return withConn(conn -> {
-				return queryInternal(sql0, params);
-			});
+	public Map<String, Object> queryUniqueMapByMap(String sql, Map<String, ?> params) {
+		List<Map<String, Object>> array = queryMapByMap(sql, params);
+		int size = array.size();
+		if (size == 0) {
+			return null;
+		} else if (size > 1) {
+			throw new RuntimeException("查询结果" + size + "条，预期1条");
 		}
-		return queryInternal(sql, params);
+		return array.get(0);
 	}
 
-	/**
-	 * 执行查询，sql支持#{}和${}占位符功能。必须在withTx或者withConn方法的回调中执行
-	 * 
-	 * @param sql
-	 * @param params
-	 * @return
-	 */
-	private JSONArray queryInternal(String sql, JSONObject params) {
-		JSONObject parseRes = null;
-		if (params != null) {
-			// 设置参数
-			parseRes = parseSql(sql, params);
-			sql = parseRes.getString("sql");
+	public <T> List<T> query(String sql, Class<T> clazz, Object... params) {
+		List<Map<String, Object>> list = queryMap(sql, params);
+		JSONArray array = (JSONArray) JSON.toJSON(list);
+		return array.toJavaList(clazz);
+	}
+
+	public List<Map<String, Object>> queryMap(String sql, Object... params) {
+		if (hasConn()) {
+			return queryMapInternal(sql, params);
 		}
-		logger.info("sql=>{}", sql);
-		logger.info("params=>{}", params);
-		Connection conn = getExistsConn();
-		try (PreparedStatement ps = conn.prepareStatement(sql);) {
-			if (params != null) {
-				JSONArray names = parseRes.getJSONArray("names");
-				for (int i = 0; i < names.size(); i++) {
-					ps.setObject(i + 1, params.get(names.get(i)));
-				}
+		return withConn(conn -> {
+			return queryMapInternal(sql, params);
+		});
+	}
+
+	private List<Map<String, Object>> queryMapInternal(String sql, Object... params) {
+		SqlPlaceHolder holder = parseSqlIndexedPlaceHolder(sql, params);
+		return queryMap0(holder);
+	}
+
+	public <T> List<T> queryByMap(String sql, Class<T> clazz, Map<String, ?> params) {
+		List<Map<String, Object>> list = queryMapByMap(sql, params);
+		JSONArray array = (JSONArray) JSON.toJSON(list);
+		return array.toJavaList(clazz);
+	}
+
+	public List<Map<String, Object>> queryMapByMap(String sql, Map<String, ?> params) {
+		if (hasConn()) {
+			return queryMapByMapInternal(sql, params);
+		}
+		return withConn(conn -> {
+			return queryMapByMapInternal(sql, params);
+		});
+	}
+
+	private List<Map<String, Object>> queryMapByMapInternal(String sql, Map<String, ?> params) {
+		SqlPlaceHolder holder = parseSqlNamedPlaceHolder(sql, params);
+		return queryMap0(holder);
+	}
+
+	private List<Map<String, Object>> queryMap0(SqlPlaceHolder holder) {
+		logger.info("sql=>{}", holder.sql);
+		logger.info("params=>{}", holder.strValues);
+		Stack<Connection> stack = connStackHolder.get();
+		Connection conn = stack.peek();
+		try (PreparedStatement ps = conn.prepareStatement(holder.sql);) {
+			int i0 = 1;
+			for (Object value : holder.values) {
+				ps.setObject(i0++, value);
 			}
 			ResultSet rs = ps.executeQuery();
 			ResultSetMetaData rsmd = rs.getMetaData();
 			int cc = rsmd.getColumnCount();
-			JSONArray list = new JSONArray();
+			List<Map<String, Object>> list = new ArrayList<>();
 			while (rs.next()) {
-				JSONObject map = new JSONObject();
+				Map<String, Object> map = new HashMap<>();
 				for (int i = 1; i <= cc; i++) {
 					String label = rsmd.getColumnLabel(i);
 					String key = Beans.underline2camel(label);
@@ -252,82 +258,158 @@ public class DbClient {
 			return list;
 		} catch (Exception e) {
 			throw new RuntimeException(e);
-		} finally {
 		}
 	}
 
-	private JSONObject parseSql(String sql, JSONObject params) {
-		JSONObject res = new JSONObject();
-		JSONArray names = new JSONArray();
+	private SqlPlaceHolder parseSqlIndexedPlaceHolder(String sql, Object... params) {
+		SqlPlaceHolder res = new SqlPlaceHolder();
+		if (params == null) {
+			res.sql = sql;
+			return res;
+		}
+		List<Object> values = new ArrayList<>();
+		Int index = new Int(0);
 		sql = new GenericTokenParser("#{", "}", (token) -> {
-			names.add(token);
+			values.add(params[index.getAndInc()]);
 			return "?";
 		}).parse(sql);
 		sql = new GenericTokenParser("${", "}", (token) -> {
-			return String.valueOf(params.get(token));
+			String trimedToken = token.trim();
+			if (trimedToken.equals("")) {
+				return String.valueOf(params[index.getAndInc()]);
+			} else {
+				return String.valueOf(params[Integer.parseInt(trimedToken)]);
+			}
 		}).parse(sql);
-		res.put("sql", sql);
-		res.put("names", names);
+		res.sql = sql;
+		res.values = values;
+		res.strValues = values.stream().map(v -> String.valueOf(v)).collect(Collectors.joining(", "));
 		return res;
 	}
-/*	public <T> T insert(String sql,JSONObject params){
-			
-	}*/
-	/**
-	 * 执行更新。必须在withTx或者withConn方法的回调中执行
-	 * 
-	 * @param sql
-	 * @return
-	 */
-	public int update(String sql) {
-		return update(sql, null);
+
+	private static class Int {
+		int value;
+
+		public Int(int value) {
+			this.value = value;
+		}
+
+		public int getAndInc() {
+			int v = value;
+			this.value++;
+			return v;
+		}
 	}
 
-	public int update(String sql, JSONObject params) {
-		if (!inWithConn && !inWithTx) {
-			String sql0 = sql;
-			return withConn(conn -> {
-				return updateInternal(sql0, params);
-			});
+	private SqlPlaceHolder parseSqlNamedPlaceHolder(String sql, Map<String, ?> params) {
+		SqlPlaceHolder res = new SqlPlaceHolder();
+		if (params == null) {
+			res.sql = sql;
+			return res;
 		}
-		return updateInternal(sql, params);
+		List<Object> values = new ArrayList<>();
+		sql = new GenericTokenParser("#{", "}", (token) -> {
+			values.add(params.get(token.trim()));
+			return "?";
+		}).parse(sql);
+		sql = new GenericTokenParser("${", "}", (token) -> {
+			return String.valueOf(params.get(token.trim()));
+		}).parse(sql);
+		res.sql = sql;
+		res.values = values;
+		res.strValues = values.stream().map(v -> String.valueOf(v)).collect(Collectors.joining(", "));
+		return res;
 	}
 
-	public <T> T insert(String sql) {
-		return insert(sql,null);
+	private static class SqlPlaceHolder {
+		private String sql;
+		public List<Object> values = emptyObjectList;
+		public String strValues = "";
 	}
-	public <T> T insert(String sql, JSONObject params) {
-		if (!inWithConn && !inWithTx) {
-			String sql0 = sql;
-			return withConn(conn -> {
-				return insertInternal(sql0, params);
-			});
+
+	public int update(String sql, Object... params) {
+		if (hasConn()) {
+			return updateInternal(sql, params);
 		}
-		return insertInternal(sql, params);
+		return withConn(conn -> {
+			return updateInternal(sql, params);
+		});
 	}
-	private <T> T insertInternal(String sql, JSONObject params) {
-		JSONObject parseRes = null;
-		if (params != null) {
-			// 设置参数
-			parseRes = parseSql(sql, params);
-			sql = parseRes.getString("sql");
+
+	private int updateInternal(String sql, Object... params) {
+		SqlPlaceHolder holder = parseSqlIndexedPlaceHolder(sql, params);
+		return update0(holder);
+	}
+
+	private int update0(SqlPlaceHolder holder) {
+		logger.info("sql=>{}", holder.sql);
+		logger.info("params=>{}", holder.strValues);
+		Stack<Connection> stack = connStackHolder.get();
+		Connection conn = stack.peek();
+		try (PreparedStatement ps = conn.prepareStatement(holder.sql)) {
+			int i0 = 1;
+			for (Object value : holder.values) {
+				ps.setObject(i0++, value);
+			}
+			return ps.executeUpdate();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
 		}
-		logger.info("sql=>{}", sql);
-		logger.info("params=>{}", params);
-		Connection conn = getExistsConn();
-		try (PreparedStatement ps = conn.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS)) {
-			if (params != null) {
-				JSONArray names = parseRes.getJSONArray("names");
-				for (int i = 0; i < names.size(); i++) {
-					ps.setObject(i + 1, params.get(names.get(i)));
-				}
+	}
+
+	public int updateByMap(String sql, Map<String, ?> params) {
+		if (hasConn()) {
+			return updateByMapInternal(sql, params);
+		}
+		return withConn(conn -> {
+			return updateByMapInternal(sql, params);
+		});
+	}
+
+	public <T> T insert(String sql, Object... params) {
+		if (hasConn()) {
+			return insertInternal(sql, params);
+		}
+		return withConn(conn -> {
+			return insertInternal(sql, params);
+		});
+	}
+
+	private <T> T insertInternal(String sql, Object... params) {
+		SqlPlaceHolder holder = parseSqlIndexedPlaceHolder(sql, params);
+		return insert0(holder);
+	}
+
+	public <T> T insertByMap(String sql, Map<String, Object> params) {
+		if (hasConn()) {
+			return insertByMapInternal(sql, params);
+		}
+		return withConn(conn -> {
+			return insertByMapInternal(sql, params);
+		});
+	}
+
+	private <T> T insertByMapInternal(String sql, Map<String, Object> params) {
+		SqlPlaceHolder holder = parseSqlNamedPlaceHolder(sql, params);
+		return insert0(holder);
+	}
+
+	private <T> T insert0(SqlPlaceHolder holder) {
+		logger.info("sql=>{}", holder.sql);
+		logger.info("params=>{}", holder.strValues);
+		Stack<Connection> stack = connStackHolder.get();
+		Connection conn = stack.peek();
+		try (PreparedStatement ps = conn.prepareStatement(holder.sql, Statement.RETURN_GENERATED_KEYS)) {
+			int i0 = 1;
+			for (Object value : holder.values) {
+				ps.setObject(i0++, value);
 			}
 			ps.executeUpdate();
 			ResultSet generatedKeys = ps.getGeneratedKeys();
 			generatedKeys.next();
 			@SuppressWarnings("unchecked")
 			T id = (T) generatedKeys.getObject(1);
-			if(id == null){
+			if (id == null) {
 				throw new RuntimeException("自动生成主键为空");
 			}
 			return id;
@@ -335,45 +417,9 @@ public class DbClient {
 			throw new RuntimeException(e);
 		}
 	}
-	/**
-	 * 执行更新(自动提交)。必须在withTx或者withConn方法的回调中执行
-	 * 
-	 * @param sql
-	 * @return
-	 */
-	private int updateInternal(String sql, JSONObject params) {
-		JSONObject parseRes = null;
-		if (params != null) {
-			// 设置参数
-			parseRes = parseSql(sql, params);
-			sql = parseRes.getString("sql");
-		}
-		logger.info("sql=>{}", sql);
-		logger.info("params=>{}", params);
-		Connection conn = getExistsConn();
-		try (PreparedStatement ps = conn.prepareStatement(sql)) {
-			if (params != null) {
-				JSONArray names = parseRes.getJSONArray("names");
-				for (int i = 0; i < names.size(); i++) {
-					ps.setObject(i + 1, params.get(names.get(i)));
-				}
-			}
-			return ps.executeUpdate();
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
-	}
-	
 
-	private Connection getExistsConn() {
-		Connection conn = connectionHolder.get();
-		try {
-			if (conn == null || conn.isClosed()) {
-				throw new RuntimeException("连接未获取或连接已关闭");
-			}
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
-		return conn;
+	private int updateByMapInternal(String sql, Map<String, ?> params) {
+		SqlPlaceHolder holder = parseSqlNamedPlaceHolder(sql, params);
+		return update0(holder);
 	}
 }
