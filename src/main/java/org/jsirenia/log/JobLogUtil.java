12a72a7a975bbabb74b01e0d27e8ecde250bc3c4
zhoujiaping <421211679@qq.com>
2019-12-28 18:46:08 +0800

    1

--- src/main/java/org/jsirenia/log/JobLogUtil.java
+++ src/main/java/org/jsirenia/log/JobLogUtil.java
@@ -17,35 +17,38 @@ import org.springframework.util.StringUtils;
  *
  */
 public class JobLogUtil {
-	private static final Logger logger = LoggerFactory.getLogger(JobLogUtil.class);
+	private static final Logger logger = LoggerFactory.getLogger(com.sfpay.msfs.jyd.common.util.JobLogUtil.class);
 	private static AtomicInteger taskid = new AtomicInteger(0);
 	private static AtomicInteger dataid = new AtomicInteger(0);
-	private static IntUnaryOperator updateFunction = new IntUnaryOperator() {
-		@Override
-		public int applyAsInt(int operand) {
-			if (operand >= Integer.MAX_VALUE) {
-				return 0;
-			}
-			return operand + 1;
+	private static IntUnaryOperator updateFunction = operand -> {
+		if (operand >= Integer.MAX_VALUE) {
+			return 0;
 		}
+		return operand + 1;
 	};
-	private static final String uidKey = "uid";
-	private static final String ouidKey = JobLogUtil.class.getSimpleName() + "-uid";
+	private static final String UID_KEY = "uid";
+	private static final String OUID_KEY = com.sfpay.msfs.jyd.common.util.JobLogUtil.class.getSimpleName() + "-uid";
+	private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
 
+	/**
+	 * 有一些job，不是通过调度平台发起的，而是业务模块通过spring或者quartz实现的。 这时候MDC中没有uid。
+	 * 
+	 * @param cb
+	 */
 	public static void withJob(Callback cb) {
-		String uid = MDC.get(uidKey);
+		String uid = MDC.get(UID_KEY);
 		if (StringUtils.isEmpty(uid)) {
 			try {
-				MDC.put(uidKey, UUID.randomUUID().toString().replaceAll("-", ""));
+				String datetime = LocalDateTime.now().format(formatter);
+				MDC.put(UID_KEY, datetime + UUID.randomUUID().toString().replaceAll("-", ""));
 				cb.apply();
 			} finally {
-				MDC.remove(uidKey);
+				MDC.remove(UID_KEY);
 			}
-		}else{
+		} else {
 			cb.apply();
 		}
 	}
-
 
 	/**
 	 * 给日志上下文添加信息 建议taskName取2-5个字符的简称 不保证同一个job的taskid按顺序增长，只保证递增。
@@ -56,13 +59,13 @@ public class JobLogUtil {
 			// 如果taskName是空，就采用计数的方式
 			if (StringUtils.isEmpty(taskName)) {
 				int id = taskid.getAndUpdate(updateFunction);
-				taskName = "task" + id;
+				taskName = "" + id;
 			}
-			String uid = MDC.get(uidKey);
+			String uid = MDC.get(UID_KEY);
 			if (StringUtils.isEmpty(uid)) {
 				logger.warn("跑批日志的uid为空，请检查配置。taskName=【{}】", taskName);
 			} else {
-				MDC.put(uidKey, uid + "-" + taskName);
+				MDC.put(UID_KEY, uid + "-T" + taskName);
 			}
 			cb.apply();
 		} finally {
@@ -74,13 +77,9 @@ public class JobLogUtil {
 	 * 将原来的uid保存到另一个key（JobLogUtil-uid）中
 	 */
 	private static void saveUid() {
-		String ouid = MDC.get(ouidKey);
-		// 为空，则说明还没保存过。不为空说明已经保存过。已经保存过不能再次保存，不然会有问题。
-		if (StringUtils.isEmpty(ouid)) {
-			String uid = MDC.get(uidKey);
-			if (!StringUtils.isEmpty(uid)) {
-				MDC.put(ouidKey, ouid);
-			}
+		String uid = MDC.get(UID_KEY);
+		if (!StringUtils.isEmpty(uid)) {
+			MDC.put(OUID_KEY, uid);
 		}
 	}
 
@@ -88,10 +87,10 @@ public class JobLogUtil {
 	 * 将保存到JobLogUtil-uid中的uid，还原到原来的key（uid）中
 	 */
 	private static void recoverUid() {
-		String ouid = MDC.get(ouidKey);
+		String ouid = MDC.get(OUID_KEY);
 		if (!StringUtils.isEmpty(ouid)) {
-			MDC.put(uidKey, ouid);
-			MDC.remove(ouidKey);
+			MDC.put(UID_KEY, ouid);
+			MDC.remove(OUID_KEY);
 		}
 	}
 
@@ -108,14 +107,15 @@ public class JobLogUtil {
 			// 如果dataId是空，就采用计数的方式
 			if (StringUtils.isEmpty(dataId)) {
 				int id = dataid.getAndUpdate(updateFunction);
-				dataId = "data" + id;
+				dataId = "" + id;
 			}
 			String uid = MDC.get("uid");
 			if (StringUtils.isEmpty(uid)) {
 				logger.warn("跑批日志的uid为空，请检查配置。dataId=【{}】", dataId);
 			} else {
-				MDC.put(uidKey, uid + "-" + dataId);
+				MDC.put(UID_KEY, uid + "-D" + dataId);
 			}
+			cb.apply();
 		} finally {
 			recoverUid();
 		}
